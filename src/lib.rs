4b30d710e98bc17117e1e6493b27f5201c25195d
Llandy Riveron Del Risco <llandy3d@gmail.com>
2023-06-18 15:01:06 +0200

    add SingleProcessBackend

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
+use std::sync::Mutex;
 
 #[pyclass]
 struct RedisBackend {
@@ -41,9 +42,54 @@ impl RedisBackend {
     }
 }
 
+#[pyclass]
+struct SingleProcessBackend {
+    #[pyo3(get)]
+    config: Py<PyDict>,
+    #[pyo3(get)]
+    metric: Py<PyAny>,
+    #[pyo3(get)]
+    histogram_bucket: Option<String>,
+    value: Mutex<f64>,
+}
+
+#[pymethods]
+impl SingleProcessBackend {
+    #[new]
+    fn new(config: &PyDict, metric: &PyAny, histogram_bucket: Option<String>) -> Self {
+        Self {
+            config: config.into(),
+            metric: metric.into(),
+            histogram_bucket,
+            value: Mutex::new(0.0),
+        }
+    }
+
+    fn inc(&mut self, value: f64) {
+        let mut data = self.value.lock().unwrap();
+        *data += value;
+    }
+
+    fn dec(&mut self, value: f64) {
+        let mut data = self.value.lock().unwrap();
+        *data -= value;
+    }
+
+    fn set(&mut self, value: f64) {
+        let mut data = self.value.lock().unwrap();
+        *data = value;
+    }
+
+    fn get(&self) -> f64 {
+        let data = self.value.lock().unwrap();
+        *data
+    }
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pytheus_backend_redis_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<RedisBackend>()?;
+    m.add_class::<SingleProcessBackend>()?;
     Ok(())
 }
